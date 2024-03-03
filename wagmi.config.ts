ca37e296919790aafc148abdfb58b04de52251bd
Yuri <yuripetusko@gmail.com>
2024-03-03 19:47:29 +0100

    format

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@wagmi/cli';
-import { actions, hardhat, foundry } from '@wagmi/cli/plugins';
-import { baseSepolia } from 'wagmi/chains';
+import { actions, foundry, hardhat } from '@wagmi/cli/plugins';
 import { RMRKCatalogFactoryContractAddress } from 'lib/consts/contract-addresses';
+import { baseSepolia } from 'wagmi/chains';
 
 type FoundryConfigDeployments = {
   [p: string]: `0x${string}` | Record<number, `0x${string}`> | undefined;
