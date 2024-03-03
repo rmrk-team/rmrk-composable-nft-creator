1925f7d8dc6779fc99658d4de8937d4bccaacb9c
Yuri <yuripetusko@gmail.com>
2024-03-03 20:05:15 +0100

    Use foundry dependencies the correct way

--- contracts/src/RMRKCatalog.sol
+++ contracts/src/RMRKCatalog.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GNU GPLv3
 pragma solidity ^0.8.21;
 
-import "@rmrk-team/evm-contracts/contracts/implementations/RMRKCatalogImpl.sol";
+import "@rmrk-team/evm-contracts/contracts/implementations/catalog/RMRKCatalogImpl.sol";
 import "@rmrk-team/evm-contracts/contracts/RMRK/utils/RMRKEquipRenderUtils.sol";
 
 contract RMRKNFTCatalog is RMRKCatalogImpl {
