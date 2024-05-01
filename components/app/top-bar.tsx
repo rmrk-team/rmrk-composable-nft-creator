cad43d14992116ef23945598564e9e0a91f3b5fa
Your Name <you@example.com>
2024-05-02 00:55:45 +0530

    fix:logo added with navigation to homepage issue:7

--- components/app/top-bar.tsx
+++ components/app/top-bar.tsx
@@ -1,18 +1,25 @@
-'use client'
+'use client';
 import { AccountWallet } from 'components/account/account-wallet';
-import React from 'react';
-import { Box, HStack } from 'styled-system/jsx';
-import logo from '../../lib/utils/top_bar_logo.webp'
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import React from 'react';
+import { Box, HStack } from 'styled-system/jsx';
+import logo from '../../lib/utils/top_bar_logo.webp';
 export const TopBar = () => {
-  const router = useRouter()
-  function handleClickToHome(){
+  const router = useRouter();
+  function handleClickToHome() {
     router.push('/');
   }
   return (
     <HStack p={2}>
-      <Image src={logo} onClick={handleClickToHome} style={{cursor:"pointer"}} alt="Logo" width={50} height={50} />
+      <Image
+        src={logo}
+        onClick={handleClickToHome}
+        style={{ cursor: 'pointer' }}
+        alt="Logo"
+        width={50}
+        height={50}
+      />
       <Box ml="auto">
         <AccountWallet />
       </Box>
