701d1f230072bfbd85c500874d6a36af88a0f807
Yuri <yuripetusko@gmail.com>
2024-05-02 23:05:45 +0200

    pass custom object name to filebase api so it detects proper content-type

--- app/ipfs/pin-metadata-json/route.ts
+++ app/ipfs/pin-metadata-json/route.ts
@@ -1,9 +1,12 @@
+import { createHash } from 'node:crypto';
 import { type File, FilebaseClient } from '@filebase/client';
 import type { Metadata } from '@rmrk-team/types';
 
 const filebaseClient = new FilebaseClient({
   token: process.env.FILEBASE_TOKEN || '',
 });
+
+const md5 = (str: string) => createHash('md5').update(str).digest('hex');
 
 export async function POST(request: Request) {
   try {
@@ -36,10 +39,15 @@ export async function POST(request: Request) {
 
     const thumbnailUri = thumbnailCid ? `ipfs://${thumbnailCid}` : undefined;
 
+    const objectName = `${md5(
+      `${mediaUri}${thumbnailUri}${metadataFieldsString}`,
+    )}.json`;
+
     //TODO: Handle mime type check for correct opensea metadata fields. If mediaFile mime is image, then duplicate it on `image` field, if it's not image, then add it to `animation_url` field and use thumbnail as `image` field.
     //TODO: Handle case where media (image, mediaUri, animation_url, thumbnailUri) are passed as ipfs uri instead of files
     const metadataCid = await filebaseClient.storeBlob(
       new Blob([JSON.stringify({ ...metadataFields, mediaUri, thumbnailUri })]),
+      objectName,
     );
 
     return Response.json({ metadataUri: `ipfs://${metadataCid}` });
