82e508669a26a58bf35750cb7e1c1f8ca47f06fb
Ayuilos <ekklovepeace@gmail.com>
2024-05-14 16:55:50 +0800

    fix: add `onValueChange` event handler inside `input-field-number` component

--- components/common/forms/input-field-number.tsx
+++ components/common/forms/input-field-number.tsx
@@ -1,4 +1,4 @@
-import { NumberInput } from 'components/park-ui/number-input';
+import { NumberInput, NumberInputProps } from 'components/park-ui/number-input';
 
 import { FormLabel } from 'components/park-ui/form-label';
 import { Text } from 'components/park-ui/text';
@@ -16,10 +16,18 @@ type Props = {
 export const InputFieldNumber = ({ name, label, max, min }: Props) => {
   const [field, meta, helpers] = useField(name);
 
+  const onValueChange: NumberInputProps['onValueChange'] = (details) => {
+    const newValue = !Number.isNaN(details.valueAsNumber) ? details.valueAsNumber : undefined;
+
+    if (!Object.is(newValue, field.value)) {
+      helpers.setValue(newValue);
+    }
+  };
+
   return (
     <VStack gap={1} alignItems={'flex-start'}>
       <FormLabel htmlFor="name">{label}</FormLabel>
-      <NumberInput {...field} max={max} min={min} />
+      <NumberInput {...field} max={max} min={min} onValueChange={onValueChange} />
       {meta.touched && meta.error && <Text color="red">{meta.error}</Text>}
     </VStack>
   );
